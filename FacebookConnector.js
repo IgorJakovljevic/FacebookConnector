d4b6e51df0489355eb95a64f91102f5f134048b1
Igor Jakovljevic <igor.james.jakovljevic@gmail.com>
2015-05-14 17:58:34 -0700

    Javascript class added to project

---
+++ FacebookConnector.js
@@ -0,0 +1,26 @@
+var FacebookConnector = {
+    _appId : "",
+    
+    ready : function(appId) {
+        this._appId = '782325705214527';
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : this._appId,
+          xfbml      : true,
+          version    : 'v2.3'
+        });
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+        
+    }
+    
+    
+
+}
